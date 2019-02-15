391be30a8b91dc904eb59bb4ee2a0a9ddfce7ebb
Abraham Bueno <abueno@localhost.localdomain>
2019-02-15 10:54:39 -0600

    LogConsumer class setup completed

--- src/main/java/com/abrahambueno/cars/LogConsumer.java
+++ src/main/java/com/abrahambueno/cars/LogConsumer.java
@@ -1,11 +1,16 @@
 package com.abrahambueno.cars;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.bridge.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class LogConsumer {
-//    @RabbitListener
+    @RabbitListener(queues = CarsApplication.QUEUE_NAME)
+    public void consumeMessage(final Message cm) {
+        log.info("Received Message: {} ", cm.toString());
+    }
 
 }
